563a9b3ca54745790b59223d4fac6647f6287466
Tamas Lustyik <elveestei@gmail.com>
2018-12-03 11:23:48 +0100

    Added utility class for deferred cleanup

--- src/common/CoreMediaIO/DeviceAbstractionLayer/Devices/Sample/SamplePrefix.h
+++ src/common/CoreMediaIO/DeviceAbstractionLayer/Devices/Sample/SamplePrefix.h
@@ -48,4 +48,18 @@
 #if !defined(__SamplePrefix_h__)
 #define __SamplePrefix_h__
 
+#include <functional>
+
+class Defer {
+public:
+    Defer(std::function<void()> deferredFunc): _deferredFunc(deferredFunc) {}
+    ~Defer() { _deferredFunc(); }
+private:
+    std::function<void()> _deferredFunc;
+};
+
+#define _CONCAT(x, y) x ## y
+#define _CONCAT2(x, y) _CONCAT(x, y)
+#define DEFER(...) Defer _CONCAT2(_defer, __LINE__)( __VA_ARGS__ );
+
 #endif
