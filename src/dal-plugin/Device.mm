f01212d54a7720f22a64c46e0187269c5e75cb4c
John Boiles <johnaboiles@gmail.com>
2020-05-15 12:52:00 -0700

    Change kCMIOObjectPropertyName to match obsproject/rfcs#15

--- src/dal-plugin/Device.mm
+++ src/dal-plugin/Device.mm
@@ -95,7 +95,7 @@
 
     switch (address.mSelector) {
         case kCMIOObjectPropertyName:
-            *static_cast<CFStringRef*>(data) = CFSTR("OBS Virtual Camera Device");
+            *static_cast<CFStringRef*>(data) = CFSTR("OBS Virtual Camera");
             *dataUsed = sizeof(CFStringRef);
             break;
         case kCMIOObjectPropertyManufacturer:
@@ -103,7 +103,7 @@
             *dataUsed = sizeof(CFStringRef);
             break;
         case kCMIOObjectPropertyElementCategoryName:
-            *static_cast<CFStringRef*>(data) = CFSTR("catname");
+            *static_cast<CFStringRef*>(data) = CFSTR("Virtual Camera");
             *dataUsed = sizeof(CFStringRef);
             break;
         case kCMIOObjectPropertyElementNumberName:
